8281602a1f14b1eac2bbf09a0c73b65efeb3591e
AleBando <56078738+morrosquin@users.noreply.github.com>
2019-11-25 11:46:55 -0500

    New Project

---
+++ AleleBake/aleandlebake/src/main/java/org/launchcode/aleandlebake/AleandlebakeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.aleandlebake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleandlebakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AleandlebakeApplication.class, args);
+	}
+
+}
---
+++ AleleBake/aleandlebake/src/test/java/org/launchcode/aleandlebake/AleandlebakeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.aleandlebake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleandlebakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
