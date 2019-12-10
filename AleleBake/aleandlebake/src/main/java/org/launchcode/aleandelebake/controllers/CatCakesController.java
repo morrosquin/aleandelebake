94828c916362937d89ec821b13fe1e1bb90424c7
AleBando <56078738+morrosquin@users.noreply.github.com>
2019-12-10 12:40:36 -0500

    Added contact page and fixed home page

--- AleleBake/aleandlebake/src/main/java/org/launchcode/aleandelebake/controllers/CatCakesController.java
+++ AleleBake/aleandlebake/src/main/java/org/launchcode/aleandelebake/controllers/CatCakesController.java
@@ -1,6 +1,6 @@
 package org.launchcode.aleandelebake.controllers;
 
-import org.launchcode.aleandelebake.models.data.HomeDao;
+import org.launchcode.aleandelebake.models.data.CategoriesDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
