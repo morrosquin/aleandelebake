c98f5bffcabd127218d37f94c3881266c8ea87b7
AleBando <56078738+morrosquin@users.noreply.github.com>
2019-12-17 17:35:10 -0500

    Trying to fix contact page, still not working but closer

--- AleleBake/aleandlebake/src/main/java/org/launchcode/aleandelebake/controllers/MsgController.java
+++ AleleBake/aleandlebake/src/main/java/org/launchcode/aleandelebake/controllers/MsgController.java
@@ -5,12 +5,9 @@ import org.launchcode.aleandelebake.models.data.MsgDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("")
@@ -19,28 +16,25 @@ public class MsgController {
     @Autowired
     private MsgDao msgDao;
 
-    @RequestMapping(value = "/msg", method = RequestMethod.GET)
+    @RequestMapping(value = "msg", method = RequestMethod.GET)
     public String displayAddMsgForm(Model model) {
         model.addAttribute(new Msg());
         model.addAttribute("title", "Add Message");
-        model.addAttribute("name", "Add Name");
-        model.addAttribute("email", "Add Email");
-        model.addAttribute("date", "Add Date");
+//        model.addAttribute("name", "Add Name");
+//        model.addAttribute("email", "Add Email");
+//        model.addAttribute("date", "Add Date");
 
-        return "/index";
+        return "Contact/index";
     }
 
     @RequestMapping(value = "/msg", method = RequestMethod.POST)
-    public String processAddMsgForm(@ModelAttribute @Valid Msg newMsg,
-                                    Errors errors, Model model) {
+    public String add(Model model, @RequestParam String email, @RequestParam String name,
+                      @RequestParam String date, @RequestParam String message) {
 
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Message");
-            return "/index";
-        }
+///
+        Msg newMsg = new Msg();
         msgDao.save(newMsg);
-        return "/msg";
-        //redirect
+        return "Contact/index";
     }
 }
 
--- AleleBake/aleandlebake/src/main/java/org/launchcode/aleandelebake/models/Msg.java
+++ AleleBake/aleandlebake/src/main/java/org/launchcode/aleandelebake/models/Msg.java
@@ -16,56 +16,70 @@ public class Msg {
     private int id;
 
     @NotNull
-    @Size(min=3, max=30)
+    @Size(min = 3, max = 30)
     private String name;
 
     @NotNull
-    @Size(min=5, message = "message must not be empty")
-    private String msg;
-
-    @NotNull
-    @Size(min=8, message = "email must not be empty")
+    @Size(min = 8, message = "email must not be empty")
     private String email;
 
     @NotNull
-    @Size(min=8, message = "date must not be empty")
+    @Size(min = 8, message = "date must not be empty")
     private String date;
 
-    public Msg(String name, String msg, String email) {
+    @NotNull
+    @Size(min = 5, message = "message must not be empty")
+    private String msg;
+
+
+    public Msg(String name, String email, String date, String msg) {
         super();
         this.id = id;
         this.name = name;
-        this.msg = msg;
         this.email = email;
         this.date = date;
+        this.msg = msg;
     }
 
-    public Msg() { }
+    public Msg() {
+    }
 
-    public int getId() { return id; }
+    public int getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public String getMsg() {
         return msg;
     }
 
-    public void setMsg(String msg) { this.msg = msg; }
-
-    public String getEmail() { return email; }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setMsg(String msg) {
+        this.msg = msg;
     }
 
-    public String getDate() { return date; }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
 }
 
